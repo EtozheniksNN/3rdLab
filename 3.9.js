5177aea04436f9b0761dd3321275dfda73b07bb5
EtozheniksNN <nsaveliev0510@gmail.com>
2020-02-05 20:18:22 +0200

    added part of lab3

---
+++ 3.9.js
@@ -0,0 +1,10 @@
+
+const name = prompt("Write your name");
+const d = new Date();
+const y = d.getFullYear();
+const da = d.getDate();
+const m = d.getMonth();
+const t = da + '/' + m + '/' + y;
+document.write("Hello, "+name+". You came in "+t+".HELLO, BROOOOOO");
+
+
