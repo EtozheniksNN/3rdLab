e9edb4602ed842773f04251b166920abb98b3c69
EtozheniksNN <nsaveliev0510@gmail.com>
2020-02-07 00:05:07 +0200

    Added navigation

--- 3.5.js
+++ 3.5.js
@@ -1,4 +1,4 @@
 Now = new Date();
 const mpo = Now.getMonth();
 const mpo1 = mpo + 1;
-document.write("<font color='green'>Сегодня " + Now.getDate() + "/" + mpo1 + "/" + Now.getFullYear() + ". Вы нафига сюда приперлись в: " + Now.getHours() + ":" + Now.getMinutes() + ":"+ Now.getSeconds() + " секунд");
+document.write("<font color='green'>Today " + Now.getDate() + "/" + mpo1 + "/" + Now.getFullYear() + ". Why are you here : " + Now.getHours() + ":" + Now.getMinutes() + ":"+ Now.getSeconds() + " seconds");
